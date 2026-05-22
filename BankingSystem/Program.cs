2847ef0ff03b15bd50152c844f8ac56c42ee1611
JadElSayegh <jadalsayegh10@gmail.com>
2026-05-22 20:10:22 +0300

    account name method and account info method

--- BankingSystem/Program.cs
+++ BankingSystem/Program.cs
@@ -3,6 +3,7 @@
 class Program
 {
     static decimal balance = 0;
+    static string accountName = "";
 
     static void Main()
     {
@@ -10,6 +11,26 @@ class Program
 
         while (running)
         {
+
+            Console.WriteLine("\n=== Welcome to the Bank!===");
+            Console.WriteLine("Enter Your Account Name: ");
+
+            try
+            {
+                accountName = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(accountName))
+                {
+                    Console.WriteLine("Account name cannot be empty.");
+                    continue;
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
             DisplayMenu();
 
             Console.Write("Choose an option: ");
@@ -26,10 +47,14 @@ class Program
                     break;
 
                 case "3":
-                    ShowBalance();
+                    Transfer();
                     break;
 
-                case "4":
+                 case "4":
+                    accInfo();
+                    break;
+
+                case "5":
                     running = false;
                     Console.WriteLine("Goodbye.");
                     break;
@@ -37,17 +62,20 @@ class Program
                 default:
                     Console.WriteLine("Invalid option.");
                     break;
+
             }
         }
     }
 
+   
     static void DisplayMenu()
     {
-        Console.WriteLine("\n=== BANK MENU ===");
+        Console.WriteLine($"\n==={accountName}'s BANK MENU ===");
         Console.WriteLine("1. Deposit");
         Console.WriteLine("2. Withdraw");
-        Console.WriteLine("3. Show Balance");
-        Console.WriteLine("4. Exit");
+        Console.WriteLine("3. Transfer");
+        Console.WriteLine("4. Account Info");
+        Console.WriteLine("5. Exit");
     }
 
     static void Deposit()
@@ -104,8 +132,40 @@ class Program
         }
     }
 
-    static void ShowBalance()
+    static void Transfer()
     {
-        Console.WriteLine($"Current balance: {balance}");
+        Console.WriteLine("Enter transfer amount: ");
+
+        try
+        {
+            decimal amount = decimal.Parse(Console.ReadLine());
+
+            if (amount <= 0)
+            {
+                Console.WriteLine("Amount must be positive.");
+                return;
+            }
+
+            if (amount > balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+                return;
+            }
+
+            balance -= amount;
+
+            Console.WriteLine($"Withdrew {amount}");
+        }
+        catch
+        {
+            Console.WriteLine("Invalid number.");
+        }
+    }
+
+    static void accInfo()
+    {
+        Console.WriteLine($"\n=== Account Information ===");
+        Console.WriteLine($"Account Name: {accountName}");
+        Console.WriteLine($"Current Balance: {balance}");
     }
 }
